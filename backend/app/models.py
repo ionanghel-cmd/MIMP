bda475c3555b57e141c61d4c6281adcca4f58ce9
ionanghel-cmd <ion.anghel@motoland.md>
2026-07-22 14:59:47 +0300

    Update models.py

--- backend/app/models.py
+++ backend/app/models.py
@@ -50,3 +50,10 @@ class Comanda(Base):
     total_profit = Column(Float, default=0.0)
 
     client = relationship("Client")
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    username = Column(String, unique=True)
+    hashed_password = Column(String)
+    role = Column(String, default="operator")  # admin, operator
